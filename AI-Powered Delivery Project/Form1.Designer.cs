6c94cdc5e652c515018cd81ab2eacf43f9e178bb
AhmedHossamEldin202 <ahmedhosamesa20@gmail.com>
2025-05-10 19:24:11 +0300

    init

--- AI-Powered Delivery Project/Form1.Designer.cs
+++ AI-Powered Delivery Project/Form1.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtnOffice = new System.Windows.Forms.Button();
+            this.BtnSports = new System.Windows.Forms.Button();
+            this.BtnBooks = new System.Windows.Forms.Button();
+            this.BtnToys = new System.Windows.Forms.Button();
+            this.BtnDevices = new System.Windows.Forms.Button();
+            this.BtnFashion = new System.Windows.Forms.Button();
+            this.BtnPets = new System.Windows.Forms.Button();
+            this.BtnHealth = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
             this.panelMenu.SuspendLayout();
             this.SuspendLayout();
@@ -44,14 +44,14 @@
             // panelMenu
             // 
             this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.panelMenu.Controls.Add(this.button8);
-            this.panelMenu.Controls.Add(this.button7);
-            this.panelMenu.Controls.Add(this.button6);
-            this.panelMenu.Controls.Add(this.button5);
-            this.panelMenu.Controls.Add(this.button4);
-            this.panelMenu.Controls.Add(this.button3);
-            this.panelMenu.Controls.Add(this.button2);
-            this.panelMenu.Controls.Add(this.button1);
+            this.panelMenu.Controls.Add(this.BtnOffice);
+            this.panelMenu.Controls.Add(this.BtnSports);
+            this.panelMenu.Controls.Add(this.BtnBooks);
+            this.panelMenu.Controls.Add(this.BtnToys);
+            this.panelMenu.Controls.Add(this.BtnDevices);
+            this.panelMenu.Controls.Add(this.BtnFashion);
+            this.panelMenu.Controls.Add(this.BtnPets);
+            this.panelMenu.Controls.Add(this.BtnHealth);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
@@ -59,110 +59,125 @@
             this.panelMenu.Size = new System.Drawing.Size(220, 580);
             this.panelMenu.TabIndex = 0;
             // 
-            // button8
+            // BtnOffice
             // 
-            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button8.FlatAppearance.BorderSize = 0;
-            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button8.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button8.Location = new System.Drawing.Point(0, 500);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(220, 60);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "Office and Professional";
-            this.button8.UseVisualStyleBackColor = true;
+            this.BtnOffice.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnOffice.FlatAppearance.BorderSize = 0;
+            this.BtnOffice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnOffice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnOffice.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnOffice.Location = new System.Drawing.Point(0, 500);
+            this.BtnOffice.Name = "BtnOffice";
+            this.BtnOffice.Size = new System.Drawing.Size(220, 60);
+            this.BtnOffice.TabIndex = 7;
+            this.BtnOffice.Text = "Office and Professional";
+            this.BtnOffice.UseVisualStyleBackColor = true;
+            this.BtnOffice.Click += new System.EventHandler(this.BtnOffice_Click);
             // 
-            // button7
+            // BtnSports
             // 
-            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button7.FlatAppearance.BorderSize = 0;
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button7.Location = new System.Drawing.Point(0, 440);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(220, 60);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "Sports and Outdoor";
-            this.button7.UseVisualStyleBackColor = true;
+            this.BtnSports.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnSports.FlatAppearance.BorderSize = 0;
+            this.BtnSports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSports.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSports.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnSports.Location = new System.Drawing.Point(0, 440);
+            this.BtnSports.Name = "BtnSports";
+            this.BtnSports.Size = new System.Drawing.Size(220, 60);
+            this.BtnSports.TabIndex = 6;
+            this.BtnSports.Text = "Sports and Outdoor";
+            this.BtnSports.UseVisualStyleBackColor = true;
+            this.BtnSports.Click += new System.EventHandler(this.BtnSports_Click);
             // 
-            // button6
+            // BtnBooks
             // 
-            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button6.Location = new System.Drawing.Point(0, 380);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(220, 60);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Books and Reading";
-            this.button6.UseVisualStyleBackColor = true;
+            this.BtnBooks.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnBooks.FlatAppearance.BorderSize = 0;
+            this.BtnBooks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnBooks.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnBooks.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnBooks.Location = new System.Drawing.Point(0, 380);
+            this.BtnBooks.Name = "BtnBooks";
+            this.BtnBooks.Size = new System.Drawing.Size(220, 60);
+            this.BtnBooks.TabIndex = 5;
+            this.BtnBooks.Text = "Books and Reading";
+            this.BtnBooks.UseVisualStyleBackColor = true;
+            this.BtnBooks.Click += new System.EventHandler(this.BtnBooks_Click);
             // 
-            // button5
+            // BtnToys
             // 
-            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button5.Location = new System.Drawing.Point(0, 320);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(220, 60);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Toys and Gaming";
-            this.button5.UseVisualStyleBackColor = true;
+            this.BtnToys.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnToys.FlatAppearance.BorderSize = 0;
+            this.BtnToys.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnToys.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnToys.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnToys.Location = new System.Drawing.Point(0, 320);
+            this.BtnToys.Name = "BtnToys";
+            this.BtnToys.Size = new System.Drawing.Size(220, 60);
+            this.BtnToys.TabIndex = 4;
+            this.BtnToys.Text = "Toys and Gaming";
+            this.BtnToys.UseVisualStyleBackColor = true;
+            this.BtnToys.Click += new System.EventHandler(this.BtnToys_Click);
             // 
-            // button4
+            // BtnDevices
             // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button4.Location = new System.Drawing.Point(0, 260);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(220, 60);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Devices and Electronics";
-            this.button4.UseVisualStyleBackColor = true;
+            this.BtnDevices.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnDevices.FlatAppearance.BorderSize = 0;
+            this.BtnDevices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDevices.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDevices.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnDevices.Location = new System.Drawing.Point(0, 260);
+            this.BtnDevices.Name = "BtnDevices";
+            this.BtnDevices.Size = new System.Drawing.Size(220, 60);
+            this.BtnDevices.TabIndex = 3;
+            this.BtnDevices.Text = "Devices and Electronics";
+            this.BtnDevices.UseVisualStyleBackColor = true;
+            this.BtnDevices.Click += new System.EventHandler(this.BtnDevices_Click);
             // 
-            // button3
+            // BtnFashion
             // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button3.Location = new System.Drawing.Point(0, 200);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(220, 60);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Fashion and Beauty";
-            this.button3.UseVisualStyleBackColor = true;
+            this.BtnFashion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnFashion.FlatAppearance.BorderSize = 0;
+            this.BtnFashion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFashion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFashion.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnFashion.Location = new System.Drawing.Point(0, 200);
+            this.BtnFashion.Name = "BtnFashion";
+            this.BtnFashion.Size = new System.Drawing.Size(220, 60);
+            this.BtnFashion.TabIndex = 2;
+            this.BtnFashion.Text = "Fashion and Beauty";
+            this.BtnFashion.UseVisualStyleBackColor = true;
+            this.BtnFashion.Click += new System.EventHandler(this.BtnFashion_Click);
             // 
-            // button2
+            // BtnPets
             // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button2.Location = new System.Drawing.Point(0, 140);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(220, 60);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Pets";
-            this.button2.UseVisualStyleBackColor = true;
+            this.BtnPets.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnPets.FlatAppearance.BorderSize = 0;
+            this.BtnPets.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnPets.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPets.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnPets.Location = new System.Drawing.Point(0, 140);
+            this.BtnPets.Name = "BtnPets";
+            this.BtnPets.Size = new System.Drawing.Size(220, 60);
+            this.BtnPets.TabIndex = 1;
+            this.BtnPets.Text = "Pets";
+            this.BtnPets.UseVisualStyleBackColor = true;
+            this.BtnPets.Click += new System.EventHandler(this.BtnPets_Click);
             // 
-            // button1
+            // BtnHealth
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button1.Location = new System.Drawing.Point(0, 80);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(220, 60);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Health and Personal Care";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BtnHealth.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnHealth.FlatAppearance.BorderSize = 0;
+            this.BtnHealth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHealth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnHealth.ForeColor = System.Drawing.Color.Gainsboro;
+            this.BtnHealth.Location = new System.Drawing.Point(0, 80);
+            this.BtnHealth.Name = "BtnHealth";
+            this.BtnHealth.Size = new System.Drawing.Size(220, 60);
+            this.BtnHealth.TabIndex = 0;
+            this.BtnHealth.Text = "Health and Personal Care";
+            this.BtnHealth.UseVisualStyleBackColor = true;
+            this.BtnHealth.Click += new System.EventHandler(this.BtnHealth_Click);
             // 
             // panelLogo
             // 
@@ -191,14 +206,14 @@
 
         private System.Windows.Forms.Panel panelMenu;
         private System.Windows.Forms.Panel panelLogo;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button BtnHealth;
+        private System.Windows.Forms.Button BtnDevices;
+        private System.Windows.Forms.Button BtnFashion;
+        private System.Windows.Forms.Button BtnPets;
+        private System.Windows.Forms.Button BtnOffice;
+        private System.Windows.Forms.Button BtnSports;
+        private System.Windows.Forms.Button BtnBooks;
+        private System.Windows.Forms.Button BtnToys;
     }
 }
 
--- AI-Powered Delivery Project/Form1.cs
+++ AI-Powered Delivery Project/Form1.cs
@@ -21,6 +21,7 @@ namespace AI_Powered_Delivery_Project
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
         }
 
         // Methods
@@ -29,7 +30,7 @@ namespace AI_Powered_Delivery_Project
             int index = random.Next(themecolor.ColorList.Count);
             while (tempindex == index)
             {
-                random.Next(themecolor.ColorList.Count);
+              index = random.Next(themecolor.ColorList.Count);
             }
             tempindex = index;
             string color = themecolor.ColorList[index];
@@ -39,22 +40,71 @@ namespace AI_Powered_Delivery_Project
         {
             if (btnSender == null) {
                 if (currentButton != (Button)btnSender) {
+                    DisableButton();
                     Color color = SelectThemeColor();
                     currentButton = (Button)btnSender;
                     currentButton.BackColor = color;
                     currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+        private void DisableButton() 
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                 }
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void BtnHealth_Click(object sender, EventArgs e)
         {
-
+            ActivateButton(sender);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        
+        }
 
+        private void BtnPets_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void BtnFashion_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void BtnDevices_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void BtnToys_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void BtnBooks_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void BtnSports_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void BtnOffice_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
         }
     }
 }
