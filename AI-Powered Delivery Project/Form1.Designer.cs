15492fc93a189198333493ef54c804a996ed61dc
Hedaya23 <hedayahany17@gmail.com>
2025-05-11 05:36:17 +0300

    init

--- AI-Powered Delivery Project/Form1.Designer.cs
+++ AI-Powered Delivery Project/Form1.Designer.cs
@@ -38,8 +38,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
             this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,8 +55,9 @@
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(220, 708);
+            this.panelMenu.Size = new System.Drawing.Size(330, 1106);
             this.panelMenu.TabIndex = 0;
             // 
             // button8
@@ -67,9 +66,10 @@
             this.button8.FlatAppearance.BorderSize = 0;
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button8.Location = new System.Drawing.Point(0, 500);
+            this.button8.Location = new System.Drawing.Point(0, 783);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(220, 60);
+            this.button8.Size = new System.Drawing.Size(330, 94);
             this.button8.TabIndex = 7;
             this.button8.Text = "Office and Professional";
             this.button8.UseVisualStyleBackColor = true;
@@ -80,9 +80,10 @@
             this.button7.FlatAppearance.BorderSize = 0;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button7.Location = new System.Drawing.Point(0, 440);
+            this.button7.Location = new System.Drawing.Point(0, 689);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(220, 60);
+            this.button7.Size = new System.Drawing.Size(330, 94);
             this.button7.TabIndex = 6;
             this.button7.Text = "Sports and Outdoor";
             this.button7.UseVisualStyleBackColor = true;
@@ -93,9 +94,10 @@
             this.button6.FlatAppearance.BorderSize = 0;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button6.Location = new System.Drawing.Point(0, 380);
+            this.button6.Location = new System.Drawing.Point(0, 595);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(220, 60);
+            this.button6.Size = new System.Drawing.Size(330, 94);
             this.button6.TabIndex = 5;
             this.button6.Text = "Books and Reading";
             this.button6.UseVisualStyleBackColor = true;
@@ -106,9 +108,10 @@
             this.button5.FlatAppearance.BorderSize = 0;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button5.Location = new System.Drawing.Point(0, 320);
+            this.button5.Location = new System.Drawing.Point(0, 501);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(220, 60);
+            this.button5.Size = new System.Drawing.Size(330, 94);
             this.button5.TabIndex = 4;
             this.button5.Text = "Toys and Gaming";
             this.button5.UseVisualStyleBackColor = true;
@@ -119,9 +122,10 @@
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button4.Location = new System.Drawing.Point(0, 260);
+            this.button4.Location = new System.Drawing.Point(0, 407);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(220, 60);
+            this.button4.Size = new System.Drawing.Size(330, 94);
             this.button4.TabIndex = 3;
             this.button4.Text = "Devices and Electronics";
             this.button4.UseVisualStyleBackColor = true;
@@ -132,9 +136,10 @@
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button3.Location = new System.Drawing.Point(0, 200);
+            this.button3.Location = new System.Drawing.Point(0, 313);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(220, 60);
+            this.button3.Size = new System.Drawing.Size(330, 94);
             this.button3.TabIndex = 2;
             this.button3.Text = "Fashion and Beauty";
             this.button3.UseVisualStyleBackColor = true;
@@ -145,9 +150,10 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button2.Location = new System.Drawing.Point(0, 140);
+            this.button2.Location = new System.Drawing.Point(0, 219);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(220, 60);
+            this.button2.Size = new System.Drawing.Size(330, 94);
             this.button2.TabIndex = 1;
             this.button2.Text = "Pets";
             this.button2.UseVisualStyleBackColor = true;
@@ -158,9 +164,10 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button1.Location = new System.Drawing.Point(0, 80);
+            this.button1.Location = new System.Drawing.Point(0, 125);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(220, 60);
+            this.button1.Size = new System.Drawing.Size(330, 94);
             this.button1.TabIndex = 0;
             this.button1.Text = "Health and Personal Care";
             this.button1.UseVisualStyleBackColor = true;
@@ -171,44 +178,18 @@
             this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Size = new System.Drawing.Size(220, 80);
+            this.panelLogo.Size = new System.Drawing.Size(330, 125);
             this.panelLogo.TabIndex = 0;
-            // 
-            // button9
-            // 
-            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button9.FlatAppearance.BorderSize = 0;
-            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button9.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button9.Location = new System.Drawing.Point(220, 0);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(906, 60);
-            this.button9.TabIndex = 1;
-            this.button9.Text = "Health and Personal Care";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button10.FlatAppearance.BorderSize = 0;
-            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button10.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button10.Location = new System.Drawing.Point(220, 60);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(906, 60);
-            this.button10.TabIndex = 2;
-            this.button10.Text = "Health and Personal Care";
-            this.button10.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1126, 708);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
+            this.ClientSize = new System.Drawing.Size(1689, 1106);
             this.Controls.Add(this.panelMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -229,8 +210,6 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
     }
 }
 
